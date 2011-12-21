ea95bbfef0e2ad8cccc48a83732f0182fafdd504
Jakub Steiner <jimmac@gmail.com>
2011-12-21 20:16:45 +0100

    / ‘boxes/interactive/js/boxes.js’

--- boxes/interactive/js/boxes.js
+++ boxes/interactive/js/boxes.js
@@ -1,3 +1,21 @@
+BOXES = {
+  boxes: [ {
+    label: "Alli's Computer",
+    thumbnail: "img/boxes/alli-thumb.png",
+    fullscreen: "img/boxes/alli.jpg"
+  }, {
+    label: "OS Research",
+    thumbnail: "img/boxes/research-thumb.png",
+    fullscreen: "img/boxes/research.jpg"    
+  }],
+  os: ['fedora','ubuntu','windows','osx','unknown'],
+  addBox: function () { //create new box
+  },
+  deleteBox: function () { // delete a box
+  }
+}
+
 $(document).ready(function () {
-  console.log('working');
+  //console.log('working');
+  
 });
