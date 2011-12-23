f4da42636b4b44236316b7e0607230c0c5cc1bcf
Jakub Steiner <jimmac@gmail.com>
2011-12-23 17:45:13 +0100

    / ‘boxes/interactive/js/boxes.js’

--- boxes/interactive/js/boxes.js
+++ boxes/interactive/js/boxes.js
@@ -77,8 +77,7 @@ enterSelectMode = function () {
     $(this).find('.box').append("<input type='checkbox' name='boxes'>").click(function (e) {
       //forward the click to the checkbox
       var $target = $(e.target);
-      console.log($target, $(this));
-      if ($target.not($(this))) {
+      if ($target.not($("input",this))) {
         console.log($("input",this));
       }
       
