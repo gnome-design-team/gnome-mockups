f6d50a91cfdd962d26594388e8d15b6d8ba7c2b0
Jakub Steiner <jimmac@gmail.com>
2011-12-23 15:02:23 +0100

    / ‘boxes/interactive/js/boxes.js’

--- boxes/interactive/js/boxes.js
+++ boxes/interactive/js/boxes.js
@@ -70,13 +70,36 @@ $.fn.renderBoxes = function () {
       .attr("id", box.id)
       .addClass("box-contain")
       .append("<div class='box'>").children()
-      .css("background-image", box.thumbnail)
+      .append("<img src='box.thumbnail'>")
       .addClass(suspended)
       .parent()
       .append("<div class='label'>"+box.label+"</div>")
       .appendTo($container);
   });
 }
+
+// Grayscale w canvas method
+	function grayscale(src){
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
+		var imgObj = new Image();
+		imgObj.src = src;
+		canvas.width = imgObj.width;
+		canvas.height = imgObj.height; 
+		ctx.drawImage(imgObj, 0, 0); 
+		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		for(var y = 0; y < imgPixels.height; y++){
+			for(var x = 0; x < imgPixels.width; x++){
+				var i = (y * 4) * imgPixels.width + x * 4;
+				var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+				imgPixels.data[i] = avg; 
+				imgPixels.data[i + 1] = avg; 
+				imgPixels.data[i + 2] = avg;
+			}
+		}
+		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+		return canvas.toDataURL();
+    }
 
 
 $(document).ready(function () {
