811e5641dd7d0beccf993c1be674d8f28193af55
Jakub Steiner <jimmac@gmail.com>
2011-12-23 21:24:02 +0100

    / ‘boxes/interactive/js/boxes.js’

--- boxes/interactive/js/boxes.js
+++ boxes/interactive/js/boxes.js
@@ -91,10 +91,10 @@ enterSelectMode = function (selected) {
       if (!$target.is($("input",this))) {
         $("input",this).trigger('click');
       }
-      $(".action").sensitivize($(":checked", $content));
+      //$(".action").sensitivize($(":checked", $content));
     });
     //sensitivize the action buttons
-    $(".action").sensitivize($(":checked", $content));
+    //$(".action").sensitivize($(":checked", $content));
   });
   if(selected) {
     //if called by longpress
