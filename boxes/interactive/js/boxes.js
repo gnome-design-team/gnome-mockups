8188faa403f32c5c075f5ea2df0c52dcf74ba50d
Jakub Steiner <jimmac@gmail.com>
2012-01-04 14:35:44 +0100

    / ‘boxes/interactive/js/boxes.js’

--- boxes/interactive/js/boxes.js
+++ boxes/interactive/js/boxes.js
@@ -172,7 +172,7 @@ enterSelectMode = function (selected) {
             'duration': 5000
            }, function () {
              //deletion was actually performed
-             console.log('wha?');
+             console.log('wha?', $content);
              //$content.renderBoxes();
            });
       }).end().append("<button id='send-to' class='fr action'>Send To</button>");
