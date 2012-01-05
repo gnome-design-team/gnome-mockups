dc750ed095a70c106dd77461b3550790d6b8a4a2
Jakub Steiner <jimmac@gmail.com>
2012-01-05 14:20:18 +0100

    / ‘boxes/interactive/overlay-toolbar/js/boxes.js’

--- boxes/interactive/overlay-toolbar/js/boxes.js
+++ boxes/interactive/overlay-toolbar/js/boxes.js
@@ -174,7 +174,6 @@ $(document).ready(function () {
             $notify.empty();
           });
         });
-        $toolbar.removeClass('selectmode').children().replaceWith($previous); //restore toolbar
         
         $(":checked",$content).parents("div.box-contain").each(function () {
           var id = $(this).index();
