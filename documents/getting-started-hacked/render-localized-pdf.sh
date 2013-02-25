7996ba5ce1f48806953f6c35aabb82d060846f0d
Jakub Steiner <jimmac@gmail.com>
2013-02-25 17:38:47 +0100

    / ‘documents/getting-started-hacked/render-localized-pdf.sh’

--- documents/getting-started-hacked/render-localized-pdf.sh
+++ documents/getting-started-hacked/render-localized-pdf.sh
@@ -6,8 +6,8 @@ do
 	xml2po -a -p $file src/landing.svg > out/$lang/landing.svg
 	xml2po -a -p $file src/goa.svg > out/$lang/goa.svg
 	#create eps
-	inkscape -z -A out/$lang/gs-landing.pdf out/$lang/landing.svg
-	inkscape -z -A out/$lang/gs-goa.pdf out/$lang/goa.svg
+	inkscape -z -A out/$lang/gs-1.pdf out/$lang/landing.svg
+	inkscape -z -A out/$lang/gs-2.pdf out/$lang/goa.svg
 	pdfunite out/$lang/gs-*pdf out/$lang/getting-started.pdf
 	#remove crap
 	rm ./out/$lang/*svg
