d394345b189993779486dd6f9ac5c1417a49094b
Jakub Steiner <jimmac@gmail.com>
2013-02-25 17:36:19 +0100

    / ‘documents/getting-started-hacked/render-localized-pdf.sh’

--- documents/getting-started-hacked/render-localized-pdf.sh
+++ documents/getting-started-hacked/render-localized-pdf.sh
@@ -3,18 +3,14 @@ do
 	lang=`echo $file | sed 's/po\/\(.*\)\.po/\1/gi'`
 	echo $lang
 	mkdir -p ./out/$lang
-	xml2po -a -p $file src/gettingstarted.svg > out/$lang/gettingstarted.svg
-	xml2po -a -p $file src/cheatsheet-01.svg > out/$lang/cheatsheet-01.svg
-	xml2po -a -p $file src/cheatsheet-02.svg > out/$lang/cheatsheet-02.svg
-	xml2po -a -p $file src/cheatsheet-03.svg > out/$lang/cheatsheet-03.svg
+	xml2po -a -p $file src/landing.svg > out/$lang/landing.svg
+	xml2po -a -p $file src/goa.svg > out/$lang/goa.svg
 	#create eps
-	inkscape -z -A out/$lang/gettingstarted.pdf out/$lang/gettingstarted.svg
-	inkscape -z -A out/$lang/cheatsheet-01.pdf out/$lang/cheatsheet-01.svg
-	inkscape -z -A out/$lang/cheatsheet-02.pdf out/$lang/cheatsheet-02.svg
-	inkscape -z -A out/$lang/cheatsheet-03.pdf out/$lang/cheatsheet-03.svg
-	pdfunite out/$lang/cheatsheet-*pdf out/$lang/cheatcheet.pdf
+	inkscape -z -A out/$lang/gs-landing.pdf out/$lang/landing.svg
+	inkscape -z -A out/$lang/gs-goa.pdf out/$lang/goa.svg
+	pdfunite out/$lang/gs-*pdf out/$lang/getting-started.pdf
 	#remove crap
 	rm ./out/$lang/*svg
-	rm ./out/$lang/cheatsheet-*
+	rm ./out/$lang/gs-*
 done
 
