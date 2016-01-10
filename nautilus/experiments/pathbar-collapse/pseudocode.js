30af548ca0bd0d6fa1a7954b7d6adc8de05fc55c
Jakub Steiner <jimmac@gmail.com>
2016-01-10 14:41:40 +0100

    / ‘autilus/experiments/pathbar-collapse/index.html’ / ‘autilus/experiments/pathbar-collapse/style.css’ + ‘nautilus/experiments/pathbar-collapse/jquery.webui-popover.css’ + ‘nautilus/experiments/pathbar-collapse/jquery.webui-popover.js’ + ‘nautilus/experiments/pathbar-collapse/pseudocode.js’ + ‘nautilus/experiments/pathbar-collapse/user-home-symbolic.svg’

---
+++ nautilus/experiments/pathbar-collapse/pseudocode.js
@@ -0,0 +1,60 @@
+PTEMPLATE = ['Documents', 'Events', '2015', 'GUADEC Team Reports'];
+MENU = ['Open in new Window', 'Open in New Tab', 'Move to…', 'Copy to…', 'Add to Bookmarks', 'Rename…', 'Properties'];
+PATH = {
+  dirs: [],
+  addDir: function () {
+    if (PTEMPLATE[this.dirs.length]) {
+      this.dirs.push(PTEMPLATE[this.dirs.length]);
+    } 
+    if (!PTEMPLATE[this.dirs.length]) {
+      $("#add").attr("disabled","true");
+    }
+  },
+  setActive: function () {
+  },
+  forkFromActive: function () {
+  },
+  renderOut: function ($pb) {
+    //console.log($pb,this.dirs);
+    var h = [],
+    b = '';
+    
+    $.each(PATH.dirs, function (i,dir) {
+      h.push('<li><a href="#">'+PATH.dirs[i]+'</a></li>');
+      if (PTEMPLATE[i+1]) {
+        h.push(' <li><span class="divider">/</span></li> ');
+      }
+    });
+    $pb.html(h.join(''));
+  }
+};
+$( document ).ready(function() {
+  var $hb = $("#headerbar"),
+  $pb = $("#pathbar"),
+  buttonwhitespace = 235;
+  $('.hasPopover').webuiPopover({
+    trigger: 'manual',
+    animation: 'gnomeslide',
+    placement: 'bottom',
+    content: function () {
+      var html='<ul>';
+      $.each(MENU, function (i,item) {
+        html += "<li>"+item+"</li>";
+      });
+      html+="</ul>";
+      return html;
+    }
+  });
+  $('.hasPopover').contextmenu(function () {
+    $(this).webuiPopover('show');
+    return false;
+  });
+  $pb.css("width", $hb.width() - buttonwhitespace);
+  $("#add").click(function () {
+    PATH.addDir();
+    PATH.renderOut($("ul.pathbar"));
+  });
+  $(window).resize(function () {
+    $pb.css("width", $hb.width() - buttonwhitespace);
+  });
+});
